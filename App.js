d842a0d2ab60f86817abbec5a77d94029ef292e9
nokk0 <derachukwu9@gmail.com>
2024-04-16 10:42:29 +0100

    Add read me.txt

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>I am using Exo go CLI</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
